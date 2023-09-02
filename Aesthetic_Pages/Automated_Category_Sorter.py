2974bedeaa30e8f4560e4ccd9ee1a33976b70e7f
keabetswe motloung <kemotlo022@student.wethinkcode.co.za>
2023-09-02 19:13:37 +0200

    added intents.json for chatbot interaction

--- Aesthetic_Pages/Automated_Category_Sorter.py
+++ Aesthetic_Pages/Automated_Category_Sorter.py
@@ -1,6 +1,7 @@
 
 import tensorflow as tf
 import numpy as np
+from keras.applications.mobilenet_v2 import MobileNetV2
 from tensorflow import keras
 from tensorflow.python.keras.layers import Dense, Activation
 from tensorflow.python.keras.models import Model
@@ -38,3 +39,29 @@ preprocessed_image = [prepare_images("-mwRkx1g.jpeg")]
 predictions = mobile.predict(preprocessed_image)
 results = imagenet_utils.decode_predictions(predictions)
 results
+
+
+# ORGANIZE DATA INTO TRAIN ,VALID AND TEST DIKS
+
+os.chdir("ScriptDataBaddie")
+if os.path.isdir("train/ScriptDataBaddie/") is False:
+    os.mkdir("train")
+    os.mkdir("valid")
+    os.mkdir("test")
+
+    for i in range(0,3):
+        shutil.move(f"{i}","train")
+        os.mkdir(f"valid/{i}")
+        os.mkdir(f"test/{i}")
+
+        valid_samples = random.sample(os.listdir(f"train/{i}"),30)
+        for j in valid_samples:
+            shutil.move(f"train/{i}/{j}",f"valid/{i}")
+
+            test_samples = random.sample(os.listdir(f"train/{i}"),5)
+            for k in test_samples:
+                shutil.move(f"train/{i}/{k}",f"test/{i}")
+
+    os.chdir()
+
+    train_path = ""
