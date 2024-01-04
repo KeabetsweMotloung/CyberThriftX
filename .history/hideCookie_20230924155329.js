3491ecb472cb17ac5120b3bf89cb0235afdbb600
kemotlo022 <kemotlo022@student.wethinkcode.co.za>
2024-01-04 19:56:15 +0200

    making changes to the home page

--- .history/hideCookie_20230924155329.js
+++
@@ -1,8 +0,0 @@
-// Function to hide the cookie consent form
-function hideCookieConsent() {
-    const cookieConsent = document.getElementById('cookie-consent');
-    if (cookieConsent) {
-      cookieConsent.style.display = 'none';
-    }
-  }
-  
